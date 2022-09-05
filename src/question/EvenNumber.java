b9cead7fd44d9d5d82d7d9bea1429e8a61e95641
G <G@DESKTOP-1FFOQKL>
2022-09-05 17:03:49 +0900

    .

---
+++ src/question/EvenNumber.java
@@ -0,0 +1,22 @@
+package question;
+
+public class EvenNumber {
+	private int num;
+
+	public int process() {
+		int sum = 0;
+		for(int i = 0; i <= num; i+=2) {
+			sum += i;
+		}
+		return sum;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+}
