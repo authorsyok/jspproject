c5a6d28388de97433575ed60cf428ed8a4ad7d09
G <G@DESKTOP-1FFOQKL>
2022-09-07 10:19:39 +0900

    .

--- WebContent/0822/array4/array4Form.jsp
+++ WebContent/0822/array4/array4Form.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<jsp:useBean id="array4" class="question.Array4"></jsp:useBean>
+<jsp:setProperty property="num" name="array4"/>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +9,9 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+	<%
+		out.println("입력된 정수의 갯수는 " + array4.getNum() + "는 <br>"
+								+ array4.process());
+	%>
 </body>
 </html>
--- src/question/Array4.java
+++ src/question/Array4.java
@@ -1,5 +1,40 @@
 package question;
 
 public class Array4 {
+	private int num;
 
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	public String process() {
+		StringBuffer sb = new StringBuffer();
+		
+		int[] arr = new int[num];
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i]=(int)(Math.random()*100)+1;
+			for (int j = 0; j < i; j++) {
+				if (arr[i]==arr[j]) {
+					i--;
+				}
+			}
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			if (i==0) {
+				sb.append(arr[i]+"    ");
+			} else {
+				if (i%10==0) {
+					sb.append("<br>");
+				}
+				sb.append(arr[i]+"    ");
+			}
+		}
+		return sb.toString();
+	}
 }
